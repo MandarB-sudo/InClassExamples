9246eda28b459fe40d0600c53703b5533b73313d
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2021-09-07 11:41:29 -0500

    Progress 1

---
+++ FC_InClass_ 1/Flow Control/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Flow_Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my world application!, What is your name ? >>");
+            string name = Console.ReadLine();
+            Console.WriteLine("${name},what is your monthly income? >>");
+            string monthlyIncomeAsString = Console.ReadLine();
+            double monthlyIncome = Convert.ToDouble(monthlyIncomeAsString);
+            Console.WriteLine("${name},what is your monthly expense? >>");
+            string Monthlyexpenseasstring = Console.ReadLine();
+            double monthlyexpense = Convert.ToDouble(Monthlyexpenseasstring);
+
+            double monthlyNetIncome = monthlyIncome - monthlyexpense;
+
+
+
+
+
+            if (monthlyNetIncome >= 0) ;
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red
+            }
+        }
+
+
+    }
+}
