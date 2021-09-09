70272910f426f61955c700ae8a0ed7261ab451b6
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2021-09-09 10:52:00 -0500

    Part 2

---
+++ FlowControlExample2/FlowControlExample2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FlowControlExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randvalue = rand.Next(1, 101);
+            if (randvalue % 3==0 && randvalue % 5 ==0)
+            {
+                Console.WriteLine("Fizzbuzz");
+            }
+            else if (randvalue % 5 ==0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (randvalue % 3 ==0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(randvalue);
+            }
+        }
+    }
+}
