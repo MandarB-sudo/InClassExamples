7d00326ce74a829d08a15c45c4a18a90bbb50b15
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2021-10-05 11:27:28 -0500

    sync

---
+++ collection_lists/collection_lists/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace collection_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> favoritethings = new List<string>();
+
+            do
+            {
+
+                Console.WriteLine("Tell me your favorite things to add to my list>>");
+                string favoritething = Console.ReadLine();
+
+                favoritethings.Add(favoritething);
+
+                Console.WriteLine("Do you have another favortie thing to add ? yes or no>>");
+
+            } while (Console.ReadLine().ToLower() =="yes");
+            Random rand = new Random();
+            int randomindex = rand.Next(0, favoritethings.Count);
+        }
+    }
+}
