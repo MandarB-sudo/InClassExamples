f3289d1d8e280e08027c07c5e44ff5731ce88ea0
Mandar Bornare <Mandar.N.Bornare-1@ou.edu>
2021-10-05 11:34:48 -0500

    sync2

--- Collection_Dictionaries/Collection_Dictionaries/Program.cs
+++ Collection_Dictionaries/Collection_Dictionaries/Program.cs
@@ -11,7 +11,24 @@ namespace Collection_Dictionaries
             studentrecords.Add(2, 3.5);
             studentrecords.Add(3, 4.0);
             Console.WriteLine("Which student you want to see the gpa of ?");
+
             int studentid = Convert.ToInt32(Console.ReadLine());
+
+
+
+            foreach (int id in studentrecords.Keys)
+            {
+                Console.WriteLine(id);
+            }
+
+            Console.WriteLine();
+            string answer;
+            do
+            {
+                Console.WriteLine("Do you want to add another student to our records? yes or no >>");
+                answer = Console.ReadLine();
+
+            } while (answer.ToLower() == "yes");
             if (studentrecords.ContainsKey(studentid) == true)
             {
                 Console.WriteLine($"{studentrecords[studentid].ToString("N")}");
